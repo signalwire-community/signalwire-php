71d075100b6247404c8a984bd71107ba3ec539ec
Luca Pradovera <luca.pradovera@gmail.com>
2019-03-06 18:11:50 +0100

    Allow passing in domain via constructor

--- src/Rest/Api.php
+++ src/Rest/Api.php
@@ -5,17 +5,20 @@ class Api extends \Twilio\Rest\Api {
   const ENV_SW_HOST = "SIGNALWIRE_API_HOSTNAME";
   public $baseUrl = '';
 
-  public function __construct(Client $client) {
+  public function __construct(Client $client, $domain = "") {
     parent::__construct($client);
 
-    $domain = "";
-    if (isset($_ENV[self::ENV_SW_HOST]) && trim($_ENV[self::ENV_SW_HOST]) !== "") {
-      $domain = trim($_ENV[self::ENV_SW_HOST]);
-    } elseif (getenv(self::ENV_SW_HOST) !== false) {
-      $domain = getenv(self::ENV_SW_HOST);
-    } else {
-      throw new \Exception("Missing SIGNALWIRE_API_HOSTNAME environment variable.");
+    if ($domain == "") {
+      $domain = "";
+      if (isset($_ENV[self::ENV_SW_HOST]) && trim($_ENV[self::ENV_SW_HOST]) !== "") {
+        $domain = trim($_ENV[self::ENV_SW_HOST]);
+      } elseif (getenv(self::ENV_SW_HOST) !== false) {
+        $domain = getenv(self::ENV_SW_HOST);
+      } else {
+        throw new \Exception("Missing SIGNALWIRE_API_HOSTNAME environment variable.");
+      }
     }
+
     $this->baseUrl = "https://$domain";
   }
 }
