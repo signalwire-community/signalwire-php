281b4def776bc094a78e1e9796f199b099dffc58
Edoardo Gallo <edo91.gallo@gmail.com>
2019-04-11 14:16:17 +0200

    test coverage on blade messages

--- src/messages/BaseMessage.php
+++ src/messages/BaseMessage.php
@@ -3,11 +3,16 @@ namespace SignalWire\Messages;
 
 abstract class BaseMessage {
   public $request = array();
+  public $id;
   protected $method;
 
   protected function buildRequest($params){
+    if (!$this->id) {
+      $this->id = \SignalWire\Util\UUID::v4();
+    }
+
     $this->request = array_merge(
-      array('jsonrpc' => '2.0', 'id' => \SignalWire\Util\UUID::v4()),
+      array('jsonrpc' => '2.0', 'id' => $this->id),
       $params
     );
   }
---
+++ tests/MessagesTest.php
@@ -0,0 +1,70 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use SignalWire\Messages\Connect;
+use SignalWire\Messages\Execute;
+use SignalWire\Messages\Subscription;
+
+class MessagesTest extends TestCase
+{
+  const UUID = '6118cfc7-9192-4869-884a-052d53434e4c';
+  // public function noop(): void {}
+
+  // protected function setUp() {
+
+  // }
+
+  // protected function tearDown() {
+    // SignalWire\Handler::clear();
+  // }
+
+  public function testBladeConnectWithoutSessionId(): void {
+    $msg = new Connect('project', 'token');
+    $json = '{"jsonrpc":"2.0","id":"'.$msg->id.'","method":"blade.connect","params":{"version":{"major":2,"minor":1,"revision":0},"authentication":{"project":"project","token":"token"}}}';
+    $this->assertEquals($msg->toJson(), $json);
+  }
+
+  public function testBladeConnectWithSessionId(): void {
+    $msg = new Connect('project', 'token', 'sessId');
+    $json = '{"jsonrpc":"2.0","id":"'.$msg->id.'","method":"blade.connect","params":{"version":{"major":2,"minor":1,"revision":0},"authentication":{"project":"project","token":"token"},"sessionid":"sessId"}}';
+    $this->assertEquals($msg->toJson(), $json);
+  }
+
+  public function testBladeExecuteRequest(): void {
+    $params = array(
+      'key' => 'value',
+      'nested' => array('service' => 'test')
+    );
+    $msg = new Execute($params);
+    $json = '{"jsonrpc":"2.0","id":"'.$msg->id.'","method":"blade.execute","params":{"key":"value","nested":{"service":"test"}}}';
+    $this->assertEquals($msg->toJson(), $json);
+
+    $params = array(
+      'key' => 'value',
+      'params' => array('channels' => array('test', 'test1', 'test2')),
+    );
+    $msg = new Execute($params);
+    $json = '{"jsonrpc":"2.0","id":"'.$msg->id.'","method":"blade.execute","params":{"key":"value","params":{"channels":["test","test1","test2"]}}}';
+    $this->assertEquals($msg->toJson(), $json);
+  }
+
+  public function testBladeSubscription(): void {
+    $params = array(
+      'command' => 'add',
+      'protocol' => 'test',
+      'channels' => array('c1', 'c2', 'c3')
+    );
+    $msg = new Subscription($params);
+    $json = '{"jsonrpc":"2.0","id":"'.$msg->id.'","method":"blade.subscription","params":{"command":"add","protocol":"test","channels":["c1","c2","c3"]}}';
+    $this->assertEquals($msg->toJson(), $json);
+
+    $params = array(
+      'command' => 'remove',
+      'protocol' => 'test',
+      'channels' => array('c1', 'c2', 'c3')
+    );
+    $msg = new Subscription($params);
+    $json = '{"jsonrpc":"2.0","id":"'.$msg->id.'","method":"blade.subscription","params":{"command":"remove","protocol":"test","channels":["c1","c2","c3"]}}';
+    $this->assertEquals($msg->toJson(), $json);
+  }
+}
