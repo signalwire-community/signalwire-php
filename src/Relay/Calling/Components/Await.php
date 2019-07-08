6349f9dfeaa61c5de4dee850953815a392e76f5e
Edoardo Gallo <edo91.gallo@gmail.com>
2019-07-08 12:04:14 +0200

    Add call waitFor method and terminate all components when the call ends

---
+++ src/Relay/Calling/Components/Await.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace SignalWire\Relay\Calling\Components;
+
+use SignalWire\Relay\Calling\Call;
+use SignalWire\Relay\Calling\Notification;
+use SignalWire\Relay\Calling\Event;
+
+class Await extends BaseComponent {
+  public $eventType = Notification::State;
+  public $event;
+
+  public function __construct(Call $call) {
+    parent::__construct($call);
+
+    $this->controlId = $call->tag;
+  }
+
+  public function method() {
+    return null;
+  }
+
+  public function payload() {
+    return null;
+  }
+
+  public function notificationHandler($params) {
+    if ($this->_hasBlocker() && in_array($params->call_state, $this->_eventsToWait)) {
+      $this->completed = true;
+      $this->successful = true;
+      $this->event = new Event($params->call_state, $params);
+      ($this->blocker->resolve)();
+    }
+  }
+}
