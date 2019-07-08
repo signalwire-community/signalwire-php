8e6e3fa105a9ebde7b1c09f32e7957f311c30dce
Edoardo Gallo <edo91.gallo@gmail.com>
2019-07-08 08:12:22 +0200

    fix and improve unit tests

--- tests/relay/RelayClientTest.php
+++ tests/relay/RelayClientTest.php
@@ -2,15 +2,13 @@
 
 use PHPUnit\Framework\TestCase;
 use SignalWire\Relay\Client;
-use SignalWire\Messages\Subscription;
 
 class RelayClientTest extends TestCase
 {
   protected $client;
-  private $_opts = array("host" => "host", "project" => "project", "token" => "token");
 
   protected function setUp() {
-    $this->client = new Client(array('host' => 'host', 'project' => 'project', 'token' => 'token'));
+    $this->client = new Client(array('project' => 'project', 'token' => 'token'));
   }
 
   public function tearDown() {
@@ -64,15 +62,5 @@ class RelayClientTest extends TestCase
     $this->assertArrayHasKey('protonotifications', $this->client->subscriptions);
     $this->assertTrue(SignalWire\Handler::isQueued('proto', 'notifications'));
     $this->assertEquals(SignalWire\Handler::queueCount('proto', 'notifications'), 1);
-  }
-
-  public function testCallingSetup(): void {
-    $responseProto = json_decode('{"requester_nodeid":"ad490dc4-550a-4742-929d-b86fdf8958ef","responder_nodeid":"b0007713-071d-45f9-88aa-302d14e1251c","result":{"protocol":"signalwire_calling_proto"}}');
-    $responseSubscr = json_decode('{"protocol":"signalwire_calling_proto","command":"add","subscribe_channels":["notifications"]}');
-    $this->_mockResponse([$responseProto, $responseSubscr]);
-    $relayInstance = $this->client->calling;
-
-    $this->assertArrayHasKey('signalwire_calling_protonotifications', $this->client->subscriptions);
-    $this->assertEquals(SignalWire\Handler::queueCount('signalwire_calling_proto', 'notifications'), 1);
   }
 }
