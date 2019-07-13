b44caffe2bda5a4710dd4ebd8cb11f2d8624fc7d
Edoardo Gallo <edo91.gallo@gmail.com>
2019-07-13 09:34:38 +0200

    genesis Relay Task class

---
+++ src/Relay/Constants.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace SignalWire\Relay;
+
+final class Constants {
+
+  // const Host = 'relay.signalwire.com';
+  const Host = 'relay.swire.io';
+
+  private function __construct() {
+    throw new Exception('Invalid class Constants');
+  }
+}
---
+++ src/Relay/Task.php
@@ -0,0 +1,48 @@
+<?php
+namespace SignalWire\Relay;
+
+use GuzzleHttp\Client;
+
+final class Task {
+
+  public $host;
+  public $project;
+  public $token;
+  public $_httpClient;
+
+  public function __construct(string $project, string $token) {
+    $this->host = 'https://' . Constants::Host;
+    $this->project = $project;
+    $this->token = $token;
+
+
+    $this->_httpClient = new Client([
+      'timeout' => 5,
+      'defaults' => [
+        'auth' => [ $this->project, $this->token ]
+      ]
+    ]);
+
+  }
+
+  public function deliver(string $context, $msg) {
+    $params = [
+      'context' => $context,
+      'message' => $msg
+    ];
+    print_r($params);
+    try {
+      $uri = "{$this->host}/api/relay/private/tasks";
+      $response = $this->_httpClient->request('POST', $uri, ['json' => $params]);
+      $body = json_decode($response->getBody());
+      print_r($body);
+
+      return true;
+    } catch (\Throwable $th) {
+      print_r($th->getRequest());
+      echo PHP_EOL . $th->getMessage() . PHP_EOL;
+
+      return false;
+    }
+  }
+}
