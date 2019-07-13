1b0bb355101468f9c11509718e3e5737f54872f9
Edoardo Gallo <edo91.gallo@gmail.com>
2019-07-13 11:06:00 +0200

    task events have message instead of params

--- src/Relay/Tasking/Tasking.php
+++ src/Relay/Tasking/Tasking.php
@@ -7,12 +7,11 @@ use SignalWire\Log;
 class Tasking extends \SignalWire\Relay\BaseRelay {
 
   public function notificationHandler($notification): void {
-    // $notification->params->event_type = $notification->event_type;
     switch ($notification->event_type)
     {
       case Notification::Receive:
         Log::info("Receive task in context: {$notification->context}");
-        Handler::trigger($this->client->relayProtocol, $notification->params, $this->_prefixCtx($notification->context));
+        Handler::trigger($this->client->relayProtocol, $notification->message, $this->_prefixCtx($notification->context));
         break;
     }
   }
