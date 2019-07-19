97baced861a627052ca55c5af2ee46a39ff5bcf2
Edoardo Gallo <edo91.gallo@gmail.com>
2019-07-19 10:19:48 +0200

    add all Detect states and make components configurable

--- src/Relay/Calling/DetectState.php
+++ src/Relay/Calling/DetectState.php
@@ -5,6 +5,13 @@ namespace SignalWire\Relay\Calling;
 final class DetectState {
   const Error = 'error';
   const Finished = 'finished';
+  // const CED = 'CED';
+  // const CNG = 'CNG';
+  // const Machine = 'MACHINE';
+  const Human = 'HUMAN';
+  // const Unknown = 'UNKNOWN';
+  // const Ready = 'READY';
+  // const NotReady = 'NOT_READY';
 
   private function __construct() {
     throw new Exception('Invalid class DetectState');
