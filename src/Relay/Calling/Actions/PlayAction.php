636d80806b6438a44a136a1dbe655232b5e2748b
Edoardo Gallo <edo91.gallo@gmail.com>
2019-09-19 17:37:05 +0200

    wip: pause/resume relay methods

--- src/Relay/Calling/Actions/PlayAction.php
+++ src/Relay/Calling/Actions/PlayAction.php
@@ -3,6 +3,8 @@
 namespace SignalWire\Relay\Calling\Actions;
 
 use SignalWire\Relay\Calling\Results\PlayResult;
+use SignalWire\Relay\Calling\Results\PlayPauseResult;
+use SignalWire\Relay\Calling\Results\PlayResumeResult;
 
 class PlayAction extends BaseAction {
 
@@ -12,6 +14,14 @@ class PlayAction extends BaseAction {
 
   public function stop() {
     return $this->component->stop();
+  }
+
+  public function pause() {
+    return $this->component->pause(PlayPauseResult::class);
+  }
+
+  public function resume() {
+    return $this->component->resume(PlayResumeResult::class);
   }
 
 }
--- src/Relay/Calling/Components/Controllable.php
+++ src/Relay/Calling/Components/Controllable.php
@@ -3,13 +3,33 @@
 namespace SignalWire\Relay\Calling\Components;
 
 use SignalWire\Messages\Execute;
+use SignalWire\Relay\Calling\Results\BaseResult;
+// use SignalWire\Relay\Calling\Event;
 
 abstract class Controllable extends BaseComponent {
 
   public function stop() {
+    return $this->_execute("{$this->method()}.stop");
+  }
+
+  public function pause(BaseResult $resultKlass) {
+    return $this->_execute("{$this->method()}.pause")->then(function() use ($resultKlass) {
+      // $this->event = new Event('', []); // TODO: use the execute response?
+      return new $resultKlass($this);
+    });
+  }
+
+  public function resume(BaseResult $resultKlass) {
+    return $this->_execute("{$this->method()}.resume")->then(function() use ($resultKlass) {
+      // $this->event = new Event('', []); // TODO: use the execute response?
+      return new $resultKlass($this);
+    });
+  }
+
+  private function _execute(string $method) {
     $msg = new Execute([
       'protocol' => $this->call->relayInstance->client->relayProtocol,
-      'method' => "{$this->method()}.stop",
+      'method' => $method,
       'params' => [
         'node_id' => $this->call->nodeId,
         'call_id' => $this->call->id,
@@ -25,5 +45,4 @@ abstract class Controllable extends BaseComponent {
       ];
     });
   }
-
 }
