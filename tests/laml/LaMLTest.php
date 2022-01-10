3d891eb8762a821c881267106a581da7592f268f
Edoardo Gallo <edo91.gallo@gmail.com>
2022-01-10 11:53:08 +0100

    write specs for MessageResponse and MessagingResponse

--- tests/laml/LaMLTest.php
+++ tests/laml/LaMLTest.php
@@ -10,4 +10,18 @@ class LaMLTest extends TestCase
     $response->play("https://ccrma.stanford.edu/~jos/mp3/gtr-nylon22.mp3", array("loop" => 5));
     $this->assertEquals($response->__toString(), "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<Response><Say>Hey!</Say><Play loop=\"5\">https://ccrma.stanford.edu/~jos/mp3/gtr-nylon22.mp3</Play></Response>\n");
   }
+
+  public function testMessageResponseLaMLMatch(): void {
+    $response = new SignalWire\LaML\MessageResponse();
+    $response->message("Hello World", ['attr' => 'value']);
+    $response->redirect("foo", ['method' => 'GET']);
+    $this->assertEquals($response->__toString(), "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<Response><Message attr=\"value\">Hello World</Message><Redirect method=\"GET\">foo</Redirect></Response>\n");
+  }
+
+  public function testMessagingResponseLaMLMatch(): void {
+    $response = new SignalWire\LaML\MessagingResponse();
+    $response->message("Hello World", ['attr' => 'value']);
+    $response->redirect("foo", ['method' => 'GET']);
+    $this->assertEquals($response->__toString(), "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<Response><Message attr=\"value\">Hello World</Message><Redirect method=\"GET\">foo</Redirect></Response>\n");
+  }
 }
