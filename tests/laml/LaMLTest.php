de703bf62369ce676d2dd1763b5b1a5c84c13af4
Edoardo Gallo <edo91.gallo@gmail.com>
2022-01-10 12:09:23 +0100

    add spec coverage for other LaML objects

--- tests/laml/LaMLTest.php
+++ tests/laml/LaMLTest.php
@@ -11,6 +11,126 @@ class LaMLTest extends TestCase
     $this->assertEquals($response->__toString(), "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<Response><Say>Hey!</Say><Play loop=\"5\">https://ccrma.stanford.edu/~jos/mp3/gtr-nylon22.mp3</Play></Response>\n");
   }
 
+  public function testFaxResponseLaMLMatch(): void {
+    $response = new SignalWire\LaML\FaxResponse();
+    $response->receive([
+      'attr' => 'value',
+      'key' => 'foo'
+    ]);
+    $this->assertEquals($response->__toString(), "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<Response><Receive attr=\"value\" key=\"foo\"/></Response>\n");
+  }
+
+  public function testVoiceResponseLaMLMatch(): void {
+    $response = new SignalWire\LaML\VoiceResponse();
+    $response->connect([
+      'field' => 'what',
+    ]);
+    $this->assertEquals($response->__toString(), "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<Response><Connect field=\"what\"/></Response>\n");
+
+    $response = new SignalWire\LaML\VoiceResponse();
+    $response->dial('+12345', [
+      'field' => 'what',
+    ]);
+    $this->assertEquals($response->__toString(), "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<Response><Dial field=\"what\">+12345</Dial></Response>\n");
+
+    $response = new SignalWire\LaML\VoiceResponse();
+    $response->enqueue('Foo', [
+      'field' => 'what',
+    ]);
+    $this->assertEquals($response->__toString(), "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<Response><Enqueue field=\"what\">Foo</Enqueue></Response>\n");
+
+    $response = new SignalWire\LaML\VoiceResponse();
+    $response->gather([
+      'field' => 'what',
+    ]);
+    $this->assertEquals($response->__toString(), "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<Response><Gather field=\"what\"/></Response>\n");
+
+    $response = new SignalWire\LaML\VoiceResponse();
+    $response->hangup();
+    $this->assertEquals($response->__toString(), "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<Response><Hangup/></Response>\n");
+
+    $response = new SignalWire\LaML\VoiceResponse();
+    $response->leave();
+    $this->assertEquals($response->__toString(), "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<Response><Leave/></Response>\n");
+
+    $response = new SignalWire\LaML\VoiceResponse();
+    $response->pause([
+      'field' => 'what',
+    ]);
+    $this->assertEquals($response->__toString(), "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<Response><Pause field=\"what\"/></Response>\n");
+
+    $response = new SignalWire\LaML\VoiceResponse();
+    $response->play('some-url-here', [
+      'field' => 'what',
+    ]);
+    $this->assertEquals($response->__toString(), "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<Response><Play field=\"what\">some-url-here</Play></Response>\n");
+
+    $response = new SignalWire\LaML\VoiceResponse();
+    $response->queue('Name', [
+      'field' => 'what',
+    ]);
+    $this->assertEquals($response->__toString(), "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<Response><Queue field=\"what\">Name</Queue></Response>\n");
+
+    $response = new SignalWire\LaML\VoiceResponse();
+    $response->record([
+      'field' => 'what',
+    ]);
+    $this->assertEquals($response->__toString(), "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<Response><Record field=\"what\"/></Response>\n");
+
+    $response = new SignalWire\LaML\VoiceResponse();
+    $response->redirect('redirect-to',[
+      'field' => 'what',
+    ]);
+    $this->assertEquals($response->__toString(), "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<Response><Redirect field=\"what\">redirect-to</Redirect></Response>\n");
+
+    $response = new SignalWire\LaML\VoiceResponse();
+    $response->reject([
+      'field' => 'what',
+    ]);
+    $this->assertEquals($response->__toString(), "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<Response><Reject field=\"what\"/></Response>\n");
+
+    $response = new SignalWire\LaML\VoiceResponse();
+    $response->say('Hello!',[
+      'field' => 'what',
+    ]);
+    $this->assertEquals($response->__toString(), "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<Response><Say field=\"what\">Hello!</Say></Response>\n");
+
+    $response = new SignalWire\LaML\VoiceResponse();
+    $response->sms('body-here',[
+      'field' => 'what',
+    ]);
+    $this->assertEquals($response->__toString(), "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<Response><Sms field=\"what\">body-here</Sms></Response>\n");
+
+    $response = new SignalWire\LaML\VoiceResponse();
+    $response->pay([
+      'field' => 'what',
+    ]);
+    $this->assertEquals($response->__toString(), "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<Response><Pay field=\"what\"/></Response>\n");
+
+    $response = new SignalWire\LaML\VoiceResponse();
+    $response->prompt([
+      'field' => 'what',
+    ]);
+    $this->assertEquals($response->__toString(), "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<Response><Prompt field=\"what\"/></Response>\n");
+
+    $response = new SignalWire\LaML\VoiceResponse();
+    $response->start([
+      'field' => 'what',
+    ]);
+    $this->assertEquals($response->__toString(), "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<Response><Start field=\"what\"/></Response>\n");
+
+    $response = new SignalWire\LaML\VoiceResponse();
+    $response->stop();
+    $this->assertEquals($response->__toString(), "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<Response><Stop/></Response>\n");
+
+
+    $response = new SignalWire\LaML\VoiceResponse();
+    $response->refer([
+      'field' => 'what',
+    ]);
+    $this->assertEquals($response->__toString(), "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<Response><Refer field=\"what\"/></Response>\n");
+  }
+
   public function testMessageResponseLaMLMatch(): void {
     $response = new SignalWire\LaML\MessageResponse();
     $response->message("Hello World", ['attr' => 'value']);
